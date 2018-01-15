e1d8da331dd7a1cce4922ba80769b569a77842e1
liuziangexit <liuziang@liuziangexit.com>
2018-01-15 18:23:27 +0800

    liuzianglib beta 2.4.22,20180115
    DC_type修改；新增DC_iterator、DC_array、DC_vector、DC_list；

--- DC_type.h
+++ DC_type.h
@@ -1,16 +1,16 @@
 #pragma once
 #ifndef liuzianglib_type
 #define liuzianglib_type
-#include <stdint.h>
-//Version 2.4.21V32
-//20170920
+//Version 2.4.22
+//20180115
 
 namespace DC {
 
-	using byte_t = unsigned char;
-	using size_t = unsigned int;
+	using byte_t = char;
+	using size_t = unsigned long long;
 	using ulong_t = unsigned long;
-	typedef int32_t pos_type;
+	using long_t = long;
+	using pos_type = int;
 
 }
 
