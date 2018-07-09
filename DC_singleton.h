60c5738660e97b57c1f054ffbc4dd5b6f9829d61
liuziangexit <liuziang@liuziangexit.com>
2018-07-09 19:54:23 +0800

    liuzianglib beta 2.4.22V22,20180709
    新增singleton；

--- DC_singleton.h
+++ DC_singleton.h
@@ -5,43 +5,43 @@
 #include <atomic>
 #include <mutex>
 #include <memory>
-//Version 2.4.22V3
-//20180116
+//Version 2.4.22V22
+//20180709
 
 namespace DC {
-    
-    template <typename T, typename LockT = std::mutex, typename AllocatorT = std::allocator<T>>
-    class singleton final {
-    public:
-        singleton() {}
-        
-        singleton(const singleton&) = delete;
-        
-        void* operator new(std::size_t) = delete;
-        
-        void operator delete(void*) = delete;
-        
-    public:
-        template <typename ...Args>
-        T* get_instance(Args ...args) {
-            if(m_object == nullptr) {
-                std::atomic_thread_fence(std::memory_order_acquire);
-                std::unique_lock<LockT> ulocker(m_lock);
-                if(m_object == nullptr) {
-                    std::atomic_thread_fence(std::memory_order_release);
-                    m_object = new(m_allocator.allocate(1)) T(std::forward<Args>(args)...);
-                }
-            }
-            return m_object;
-        }
-        
-        void clear();
-        
-    private:
-        T *m_object {nullptr};
-        LockT m_lock;
-        AllocatorT m_allocator;
-    };
+
+	template <typename T, typename LockT = std::mutex, typename AllocatorT = std::allocator<T>>
+	class singleton final {
+	public:
+		singleton() {}
+
+		singleton(const singleton&) = delete;
+
+		void* operator new(std::size_t) = delete;
+
+		void operator delete(void*) = delete;
+
+	public:
+		template <typename ...Args>
+		T* get_instance(Args ...args) {
+			if (m_object == nullptr) {
+				std::atomic_thread_fence(std::memory_order_acquire);
+				std::unique_lock<LockT> ulocker(m_lock);
+				if (m_object == nullptr) {
+					std::atomic_thread_fence(std::memory_order_release);
+					m_object = new(m_allocator.allocate(1)) T(std::forward<Args>(args)...);
+				}
+			}
+			return m_object;
+		}
+
+		void clear();
+
+	private:
+		T *m_object{ nullptr };
+		LockT m_lock;
+		AllocatorT m_allocator;
+	};
 
 }
 
